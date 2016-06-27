6a520b6cdaddd34830ff6a45d2f85fca238963a8
Thimo Jansen <thimo@defrog.nl>
2016-06-27 15:35:21 +0200

    Load lib

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module Schottenco
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :nl
+
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
